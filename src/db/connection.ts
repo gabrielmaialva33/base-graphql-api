75e3211346c0fe0b98a08ae1adb9ff81d51726ea
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-07-03 11:02:22 -0300

    refactor: sql logs

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,8 +1,14 @@
+import { LOG } from '@utils/log'
 import Knex from 'knex'
 import KnexTinyLogger from 'knex-tiny-logger'
 
 const db = KnexTinyLogger(Knex(require('../config').Knex.config), {
   bindings: true,
+  logger: (_?: any, ...log: any[]) => {
+    let l = ''
+    for (const line of log) l += line + ' '
+    return LOG('info', l)
+  },
 })
 
 export default db
