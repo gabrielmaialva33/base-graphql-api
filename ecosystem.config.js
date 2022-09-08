09b1348c2fc373703851863a33fc358df817efb2
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-09-08 01:21:02 -0300

    chore: config pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'base-graphql-api',
-      script: './build/server.js',
+      script: 'node -r ts-node/register/transpile-only -r tsconfig-paths/register build/server.js',
       instances: 'max',
       exec_mode: 'cluster',
       autorestart: true,
